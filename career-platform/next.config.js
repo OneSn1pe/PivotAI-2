8568fc0fdb779cb712048548311ab3182c3d97db
Kaustubh Kislay <kaustubh.kislay@gmail.com>
2025-04-20 05:23:49 -0400

    simplification + error handling

--- career-platform/next.config.js
+++ career-platform/next.config.js
@@ -55,8 +55,8 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // Apply these headers to all routes
-        source: '/:path*',
+        // Apply these headers to all API routes
+        source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
