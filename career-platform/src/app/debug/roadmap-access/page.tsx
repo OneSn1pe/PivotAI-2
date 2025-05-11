85e99e5500328df8a71fd5182a4396971541b92d
Kaustubh Kislay <kaustubh.kislay@gmail.com>
2025-05-11 04:37:37 -0400

    debugging

---
+++ career-platform/src/app/debug/roadmap-access/page.tsx
@@ -0,0 +1,11 @@
+import RoadmapAccessDebugPage from './RoadmapAccessDebugPage';
+import FirestoreRulesTest from './firestore-rules-test';
+
+export default function RoadmapAccessDebugContainer() {
+  return (
+    <>
+      <RoadmapAccessDebugPage />
+      <FirestoreRulesTest />
+    </>
+  );
+} 
